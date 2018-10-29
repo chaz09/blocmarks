cfd2f43f196e8e7fd205379ff7cb53984b5ed22a
Charlyn Oyakhilome <charlynoyakhilome@gmail.com>
2018-10-29 15:01:16 +0000

    reauthorising mail gun with a new repo

---
+++ config/initializers/mail.rb
@@ -0,0 +1,17 @@
+ActionMailer::Base.smtp_settings = {
+  port:              587,
+  address:           'smtp.mailgun.org',
+  user_name:         ENV['MAILGUN_SMTP_LOGIN'],
+  password:          ENV['MAILGUN_SMTP_PASSWORD'],
+  domain:            '',
+  authentication:    :plain,
+  content_type:      'text/html'
+}
+ActionMailer::Base.delivery_method = :smtp
+
+# Makes debugging *way* easier.
+ActionMailer::Base.raise_delivery_errors = true
+
+# This interceptor just makes sure that local mail
+# only emails you.
+# http://edgeguides.rubyonrails.org/action_mailer_basics.html#intercepting-emails
---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+
+  devise_for :users
+
+
+  resources :topics do
+  resources :bookmarks
+end
+
+  post :incoming, to: 'incoming#create'
+
+
+  get 'welcome/index'
+
+  get 'welcome/about'
+
+  root 'welcome#index'
+end
